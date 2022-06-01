5da3fbbb24872e0a562838baa2e9ba2aaaac3452
swapnilkrish <86659449+swapnilkrish@users.noreply.github.com>
2022-06-01 15:38:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function setup() {
   canvas.center();
   video = createCapture(VIDEO);
   video.hide();
-  classifier = m5.imageClassifier('MobileNet', modelLoaded);
+  classifier = ml5.imageClassifier('MobileNet', modelLoaded);
 }
 
 function modelLoaded() {
@@ -11,8 +11,8 @@ function modelLoaded() {
 }
 
 function draw() {
-  Image(video, 0, 0, 300, 300);
-  classifier.classify(video, gotResults);
+  image(video, 0, 0, 300, 300);
+  classifier.classify(video, gotResult);
 }
 var previous_result = '';
 
@@ -20,13 +20,15 @@ function gotResult(error, results) {
   if (error) {
     console.error(error);
   } else {
-    if ((results[0].confidence > 0.5) && (previous_results != results[0].label)) {
+    if ((results[0].confidence > 0.5) && (previous_result != results[0].label)) {
       console.log(results);
-      previous_results = results[0].label;
-      var synth = window.SpeechSynthesisUtterance(speak_data);
+      previous_result = results[0].label;
+      var synth = window.speechSynthesis;
+      speak_data = 'Object detected is - ' + results[0].label;
+      var utterThis = new SpeechSynthesisUtterance(speak_data);
       synth.speak(utterThis);
 
-      document.getElementById("results_object_name").innerHTML = results[0].label;
+      document.getElementById("result_object_name").innerHTML = results[0].label;
       document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
     }
   }
